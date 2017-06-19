24ee76d067ed4cf0e7b0533181d560fe965fcbdc
Andre Christoga <andrechristoga@gmail.com>
2017-06-19 18:49:23 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBqvr0hrwvYUCws8ut8z3u1dbDWD0XuHbM",
+  authDomain: "gora-233f7.firebaseapp.com",
+  databaseURL: "https://gora-233f7.firebaseio.com",
+  projectId: "gora-233f7",
+  storageBucket: "gora-233f7.appspot.com",
+  messagingSenderId: "743066077702"
+};
+firebase.initializeApp(config);
+// var provider = new firebase.auth.FacebookAuthProvider();
+
+// Call user personal data
+// var nama = provider.addScope('public_profile.name');
+// var gender = provider.addScope('public_profile.gender');
+// var lokasi = provider.addScope('user_location');
+
